e8c26f83c53bf835e5e491785d10aaf4aec0eb6e
Nick Tsai <myintaer@gmail.com>
2018-03-10 17:52:49 +0800

    Release version 1.1.3

--- src/phpSpreadsheet/Helper.php
+++ src/phpSpreadsheet/Helper.php
@@ -8,7 +8,7 @@ use Exception;
  * PhpSpreadsheet Helper
  * 
  * @author      Nick Tsai <myintaer@gmail.com>
- * @version     1.1.1
+ * @version     1.1.3
  * @filesource 	PhpSpreadsheet <https://github.com/PHPOffice/PhpSpreadsheet>
  * @see         https://github.com/yidas/phpspreadsheet-helper
  * @example
